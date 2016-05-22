ec484525c7aaac9812c8b1f481317a0e78c8658e
brishewatts <davor.brisevac@gmail.com>
2016-05-23 00:14:48 +0200

    popravek

--- loto.py
+++ loto.py
@@ -1,19 +1,19 @@
 import random
-# ustvari seznam kjer se shranjujejo stevilke
+
 loto_stevilke = []
 
 for i in range (0,6):
   stevilka = random.randint(1,50)
-# preveri ce izbrana stevilka ze obstaja
+
   while stevilka in loto_stevilke:
-# in ce obstaja izberi drugo stevilko
+
     stevilka = random.randint(1,50)
   
-# enkrat ko imamo stevilko katero se ne ponavlja jo dodaj v seznam
+
   loto_stevilke.append(stevilka)
 
-# pa se sortiraj jih od manjse proti vecji...
+
 loto_stevilke.sort()
-# izpisi stevilke
+
 print("Danasnje loto stevilke so: ")
 print(loto_stevilke)
